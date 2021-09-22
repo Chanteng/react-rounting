324c9c6d0654557b596b7acd7737d0ea5e15e896
Chanteng <yrnchanteng@gmail.com>
2021-09-22 11:48:30 -0700

    first commit

---
+++ src/Pages/Contatcts.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+
+ function Contacts() {
+    return (
+        <div>
+
+        <Link to="/" className="link-tap">Home</Link>
+        <Link to="/about" className="link-tap">About</Link>
+        <Link to="/contacts" className="link-tap">Contacts</Link>
+        <Link to="/services" className="link-tap">Services</Link>
+            
+        <h1>Welcome to our Contact Page</h1>
+        <p>How may I help you!</p>
+
+        
+
+        </div>
+    )
+}
+
+
+export default Contacts
